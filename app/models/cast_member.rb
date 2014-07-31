f5c2215b4f26b56b82a5907f50d19b92dcc09b5e
vagrant <vagrant@ironhack>
2014-07-31 13:22:05 +0000

    Validations made

--- app/models/cast_member.rb
+++ app/models/cast_member.rb
@@ -1,3 +1,4 @@
 class CastMember < ActiveRecord::Base
 	has_and_belongs_to_many :movies
+	validates :full_name, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
-	 has_and_belongs_to_many :cast_members
+	has_and_belongs_to_many :cast_members
+	validates :poster, presence: true
+	validates :title, uniqueness: true
 	def self.last_updated_movies(number)
 		Movie.limit(number).order(created_at: :desc)
 	end

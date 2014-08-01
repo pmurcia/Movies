6d31e289fb4d55d126825060a5acbcc117502dee
vagrant <vagrant@ironhack>
2014-08-01 16:09:36 +0000

    Added time to reviews

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,21 @@ class MoviesController < ApplicationController
 		render layout: 'movie'
 	end
 	
-	def new
-		
+	def edit
+		@movie = Movie.find params[:id]
 	end
+
+	def update
+		@movie = Movie.find params[:id]
+		if @movie.update_attributes 
+			redirect_to action: 'index', controller: 'movies', movie_id: @movie.id
+		else
+			flash[:alert] = "Something hasn't been OK..."
+			
+			@errors = @entry.errors.full_messages
+			render 'edit'
+		end
+	end
+
+	
 end
